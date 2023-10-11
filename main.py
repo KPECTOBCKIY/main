4d07deed713e0eedc3a88b69f04c0fbe15f513e6
KPECTOBCKIY <147333475+KPECTOBCKIY@users.noreply.github.com>
2023-10-11 20:42:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ bot = telebot.TeleBot('6648964856:AAEl3d48O2kL71dxEpFrK8CAgL2Oy9kLkoY');
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
 
-@bot.message_handler(content_types=['text', 'document', 'audio'])
-
 if message.text == "Привет":
     bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
 elif message.text == "/help":
